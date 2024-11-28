f24b998bd4a643b906052f1c7b1e1c263a72ee23
heegu123 <kookminkhg@kookmin.ac.kr>
2024-11-29 03:51:46 +0900

    week2 homework

---
+++ src/main/java/utils/Define.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class Define {
+
+    public static final int KOREAN = 1001;  //±¹¾î
+    public static final int MATH = 2001;    //¼öÇÐ
+    public static final int DANCE = 3001;   //¹æ¼Û´í½º
+
+    public static final int AB_TYPE = 0;    // A, B, C
+    public static final int SAB_TYPE = 1;   // S, A, B, c
+    public static final int PF_TYPE = 2;   // P, F
+
+}
